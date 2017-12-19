ea2fb73450f1b6c9fe6579906fcfc641450857fb
Christopher Dorst <chris@dorst.email>
2017-12-19 04:19:00 -0600

    Adding NuGetReference type

---
+++ DevOps.Primitives.NuGet/NuGetReference.cs
@@ -0,0 +1,15 @@
+﻿namespace DevOps.Primitives.NuGet
+{
+    public class NuGetReference
+    {
+        public NuGetReference() { }
+        public NuGetReference(string include, string version)
+        {
+            Include = include;
+            Version = version;
+        }
+
+        public string Include { get; set; }
+        public string Version { get; set; }
+    }
+}
